4ffbd6420e5d2c96a0b2f8ac163cc5033d292831
Syahrudin Sefry <109064862+sefryss@users.noreply.github.com>
2023-08-15 17:17:07 +0700

    Update digimon.freezed.dart

--- lib/src/models/digimon.freezed.dart
+++ lib/src/models/digimon.freezed.dart
@@ -14,28 +14,33 @@ T _$identity<T>(T value) => value;
 final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
 
+digimon _$digimonFromJson(Map<String, dynamic> json) {
+  return _digimon.fromJson(json);
+}
+
 /// @nodoc
-mixin _$Digimon {
+mixin _$digimon {
   String get name => throw _privateConstructorUsedError;
   String get img => throw _privateConstructorUsedError;
   String get level => throw _privateConstructorUsedError;
 
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
   @JsonKey(ignore: true)
-  $DigimonCopyWith<Digimon> get copyWith => throw _privateConstructorUsedError;
+  $digimonCopyWith<digimon> get copyWith => throw _privateConstructorUsedError;
 }
 
 /// @nodoc
-abstract class $DigimonCopyWith<$Res> {
-  factory $DigimonCopyWith(Digimon value, $Res Function(Digimon) then) =
-      _$DigimonCopyWithImpl<$Res, Digimon>;
+abstract class $digimonCopyWith<$Res> {
+  factory $digimonCopyWith(digimon value, $Res Function(digimon) then) =
+      _$digimonCopyWithImpl<$Res, digimon>;
   @useResult
   $Res call({String name, String img, String level});
 }
 
 /// @nodoc
-class _$DigimonCopyWithImpl<$Res, $Val extends Digimon>
-    implements $DigimonCopyWith<$Res> {
-  _$DigimonCopyWithImpl(this._value, this._then);
+class _$digimonCopyWithImpl<$Res, $Val extends digimon>
+    implements $digimonCopyWith<$Res> {
+  _$digimonCopyWithImpl(this._value, this._then);
 
   // ignore: unused_field
   final $Val _value;
@@ -67,20 +72,20 @@ class _$DigimonCopyWithImpl<$Res, $Val extends Digimon>
 }
 
 /// @nodoc
-abstract class _$$_DigimonCopyWith<$Res> implements $DigimonCopyWith<$Res> {
-  factory _$$_DigimonCopyWith(
-          _$_Digimon value, $Res Function(_$_Digimon) then) =
-      __$$_DigimonCopyWithImpl<$Res>;
+abstract class _$$_digimonCopyWith<$Res> implements $digimonCopyWith<$Res> {
+  factory _$$_digimonCopyWith(
+          _$_digimon value, $Res Function(_$_digimon) then) =
+      __$$_digimonCopyWithImpl<$Res>;
   @override
   @useResult
   $Res call({String name, String img, String level});
 }
 
 /// @nodoc
-class __$$_DigimonCopyWithImpl<$Res>
-    extends _$DigimonCopyWithImpl<$Res, _$_Digimon>
-    implements _$$_DigimonCopyWith<$Res> {
-  __$$_DigimonCopyWithImpl(_$_Digimon _value, $Res Function(_$_Digimon) _then)
+class __$$_digimonCopyWithImpl<$Res>
+    extends _$digimonCopyWithImpl<$Res, _$_digimon>
+    implements _$$_digimonCopyWith<$Res> {
+  __$$_digimonCopyWithImpl(_$_digimon _value, $Res Function(_$_digimon) _then)
       : super(_value, _then);
 
   @pragma('vm:prefer-inline')
@@ -90,7 +95,7 @@ class __$$_DigimonCopyWithImpl<$Res>
     Object? img = null,
     Object? level = null,
   }) {
-    return _then(_$_Digimon(
+    return _then(_$_digimon(
       name: null == name
           ? _value.name
           : name // ignore: cast_nullable_to_non_nullable
@@ -108,48 +113,71 @@ class __$$_DigimonCopyWithImpl<$Res>
 }
 
 /// @nodoc
+@JsonSerializable()
+class _$_digimon with DiagnosticableTreeMixin implements _digimon {
+  const _$_digimon({this.name = '-', this.img = '-', this.level = '-'});
 
-class _$_Digimon implements _Digimon {
-  const _$_Digimon(
-      {required this.name, required this.img, required this.level});
+  factory _$_digimon.fromJson(Map<String, dynamic> json) =>
+      _$$_digimonFromJson(json);
 
   @override
+  @JsonKey()
   final String name;
   @override
+  @JsonKey()
   final String img;
   @override
+  @JsonKey()
   final String level;
 
   @override
-  String toString() {
-    return 'Digimon(name: $name, img: $img, level: $level)';
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'digimon(name: $name, img: $img, level: $level)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'digimon'))
+      ..add(DiagnosticsProperty('name', name))
+      ..add(DiagnosticsProperty('img', img))
+      ..add(DiagnosticsProperty('level', level));
   }
 
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$_Digimon &&
+            other is _$_digimon &&
             (identical(other.name, name) || other.name == name) &&
             (identical(other.img, img) || other.img == img) &&
             (identical(other.level, level) || other.level == level));
   }
 
+  @JsonKey(ignore: true)
   @override
   int get hashCode => Object.hash(runtimeType, name, img, level);
 
   @JsonKey(ignore: true)
   @override
   @pragma('vm:prefer-inline')
-  _$$_DigimonCopyWith<_$_Digimon> get copyWith =>
-      __$$_DigimonCopyWithImpl<_$_Digimon>(this, _$identity);
+  _$$_digimonCopyWith<_$_digimon> get copyWith =>
+      __$$_digimonCopyWithImpl<_$_digimon>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_digimonToJson(
+      this,
+    );
+  }
 }
 
-abstract class _Digimon implements Digimon {
-  const factory _Digimon(
-      {required final String name,
-      required final String img,
-      required final String level}) = _$_Digimon;
+abstract class _digimon implements digimon {
+  const factory _digimon(
+      {final String name, final String img, final String level}) = _$_digimon;
+
+  factory _digimon.fromJson(Map<String, dynamic> json) = _$_digimon.fromJson;
 
   @override
   String get name;
@@ -159,6 +187,6 @@ abstract class _Digimon implements Digimon {
   String get level;
   @override
   @JsonKey(ignore: true)
-  _$$_DigimonCopyWith<_$_Digimon> get copyWith =>
+  _$$_digimonCopyWith<_$_digimon> get copyWith =>
       throw _privateConstructorUsedError;
 }
