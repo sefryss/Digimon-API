882ba98612f5c5b5bc8f85db9900c1e786955362
Syahrudin Sefry <109064862+sefryss@users.noreply.github.com>
2023-08-15 17:16:00 +0700

    Add files via upload

---
+++ lib/src/models/digimon.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'digimon.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_digimon _$$_digimonFromJson(Map<String, dynamic> json) => _$_digimon(
+      name: json['name'] as String? ?? '-',
+      img: json['img'] as String? ?? '-',
+      level: json['level'] as String? ?? '-',
+    );
+
+Map<String, dynamic> _$$_digimonToJson(_$_digimon instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'img': instance.img,
+      'level': instance.level,
+    };
