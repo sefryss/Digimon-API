f69b7e77df08e620fd32de1497a1a03900a583c3
Syahrudin Sefry <109064862+sefryss@users.noreply.github.com>
2023-08-15 17:15:19 +0700

    Update digimon_bloc.dart

--- lib/src/bloc/digimon_bloc.dart
+++ lib/src/bloc/digimon_bloc.dart
@@ -1,39 +1,36 @@
 import 'package:bloc/bloc.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:digimon_api/src/models/digimon.dart';
+import 'package:digimon_api/src/bloc/digimon_event.dart';
+import 'package:digimon_api/src/bloc/digimon_state.dart';
+import 'package:digimon_api/src/repository/digimon_repository.dart';
 
-import '../models/digimon.dart';
-import '../repository/digimon_repository.dart';
+class DigimonBloc extends Bloc<DigimonEvent, DigimonState> {
+  final DigimonRepository _repository;
 
-class DigimonState {
-  final List<Digimon> digimons;
-  final ThemeMode themeMode;
+  DigimonBloc(this._repository) : super(InitialState()) {
+    on<FetchDigimonData>((event, emit) async {
+      emit(LoadingState());
+      try {
+        final digimons = await _repository.getDigimon();
 
-  DigimonState({
-    required this.digimons,
-    required this.themeMode,
-  });
-}
+        emit(SuccessState(digimons));
+      } catch (error) {
+        print('$error unik');
+        emit(ErrorState('Failed to gets digimons'));
+      }
+    });
 
-class DigimonCubit extends Cubit<DigimonState> {
-  final DigimonRepository _repository = DigimonRepository();
-
-  DigimonCubit()
-      : super(DigimonState(digimons: [], themeMode: ThemeMode.light));
-
-  Future<void> fetchDigimons() async {
-    try {
-      final digimons = await _repository.fetchDigimons();
-      emit(DigimonState(digimons: digimons, themeMode: state.themeMode));
-    } catch (error) {
-      print('Error fetching digimons: $error'); // Tampilkan pesan kesalahan
-      throw Exception('Failed to fetch digimons');
+    @override
+    Stream<DigimonState> mapEventToState(DigimonEvent event) async* {
+      if (event is FetchDigimonData) {
+        yield LoadingState();
+        try {
+          final digimons = await _repository.getDigimon();
+          yield SuccessState(digimons);
+        } catch (error) {
+          yield ErrorState('Failed to fetch data');
+        }
+      }
     }
-  }
-
-  void toggleTheme() {
-    final newThemeMode =
-        state.themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    emit(DigimonState(digimons: state.digimons, themeMode: newThemeMode));
   }
 }
